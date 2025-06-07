893b46f68f8b813ed8627b13036eef942ea6885e
Tenteur <vianneyleman592@gmail.com>
2025-06-07 19:00:02 +0200

    feat: Created and coded quickly a parser for commented JSON

---
+++ src/jsoncParser.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+int main() {
+    char filename[256];
+    char newFilename[256];
+    char ch;
+    int isInQuote = 0b0;
+    int isAComment = 0b0;
+    int ignoreNextChar = 0b0;
+
+    FILE *file;
+    FILE *newFile;
+
+    printf("Enter the filename as .jsonc: ");
+    if (scanf("%255s", filename) != 1) {
+        printf("Failed to read filename.\n");
+        return 1;
+    }
+    printf("Enter the new filename as .json: ");
+    if (scanf("%255s", newFilename) != 1) {
+        printf("Failed to create file.\n");
+        return 1;
+    }
+
+    file = fopen(filename, "r");
+    if (!file) {
+        printf("Could not open file: %s\n", filename);
+        return 1;
+    }
+
+    newFile = fopen(newFilename, "w");
+
+    while ((ch = fgetc(file)) != EOF) {
+        if (!ignoreNextChar) { // This is the second slash from the start of the comment
+            if (ch == '"') isInQuote = isInQuote ^ 0b1;
+
+            else { // This is not a quote so can be a comment
+                if (ch == '/' && !isInQuote) {
+                    isAComment = 1;
+                    ignoreNextChar = 1;
+                }
+                if (ch == 0x0A) { // This is a newline so no more comment
+                    isAComment = 0;
+                    ignoreNextChar = 0;
+                }
+            }
+            if (!isAComment) {
+                printf("%c", ch);
+                putc(ch, newFile);
+            }
+        } else {
+            if ((ch = fgetc(file)) == EOF) break;
+            ignoreNextChar = 0;
+        }
+    }
+    // Closing the file
+
+    fclose(file);
+    printf("File closed successfully.\n");
+    return 0;
+}
