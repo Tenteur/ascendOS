aa2f4f8edf69ed48eaabe5e61261e94975b47eb1
Tenteur <vianneyleman592@gmail.com>
2025-06-10 18:48:02 +0200

    refactor: Update function documentation and add debug prints for image loading, not working most of the times (weird)

--- src/images.c
+++ src/images.c
@@ -8,7 +8,7 @@
 #include "../include/images.h"
 
 signed int imagesInit() {
-    IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG);
+    printf("%u\n", IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG));
     return 0;
 }
 
@@ -21,11 +21,22 @@ SDL_Surface *loadImageAsSurface(const char *imagePath) {
 
 signed int renderImage(SDL_Renderer *renderer, const char *imagePath, const int x, const int y) {
     SDL_Texture *imageTexture = loadImageAsTexture(renderer, imagePath);
+    if (imageTexture == NULL) {
+        printf("Failure!\n");
+        return -1;
+    } else {
+        printf("Success!\n");
+    }
     SDL_Rect tempRect;
     SDL_QueryTexture(imageTexture, NULL, NULL, &(tempRect.w), &(tempRect.h));
+    printf("1\n");
     tempRect.x = x;
+    printf("2\n");
     tempRect.y = y;
+    printf("3\n");
     SDL_RenderCopy(renderer, imageTexture, NULL, &tempRect);
+    printf("4\n");
     SDL_DestroyTexture(imageTexture);
+    printf("5\n");
     return 0;
 }
