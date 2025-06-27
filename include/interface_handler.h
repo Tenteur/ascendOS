bf79cb24ce21f0bfc47a20a4e6d5146d48d5d158
Tenteur <vianneyleman592@gmail.com>
2025-06-27 13:18:11 +0200

    feat: Add render_managerFuseTextures function for texture fusion

--- include/interface_handler.h
+++ include/interface_handler.h
@@ -5,6 +5,8 @@
  * This code aims to translate a .jsonUI file to actual rendering by using images, text and all available means.
  */
 
+#include <SDL2/SDL.h>
+
 /**
  * @brief Init the variables and functions needed for the code to work.
  */
@@ -19,3 +21,18 @@ int interface_handlerQuit();
  * @param sceneName The name of the file where the scene is to be loader (as a .jsonUI)
  */
 int interface_handlerLoadScene(char sceneName[15]);
+
+/**
+ * @brief Fuse two textures and destroys them
+ * Create a new texture and output it to the textures' pointer (outputTexture)
+ * The second texture may be drawn over the first one
+ * @param renderer The SDL renderer.
+ * @param outputTexture The pointer of the final texture (need to be as big as the biggest of the two textures) and should already exist
+ * @param firstInputTexture The pointer of the first texture to fuse
+ * @param secondInputTexture The pointer of the second texture to fuse
+ * @param firstInputCoord The coordinate of the first texture relative to the output texture
+ * @param secondInputCoord The coordinate of the second texture relative to the output texture
+ * @param firstInputSRect The texture coordinate of the first texture relative to the output texture (can be defaulted to NULL)
+ * @param secondInputSRect The texture coordinate of the second texture relative to the output texture (can be defaulted to NULL)
+ */
+signed int render_managerFuseTextures(SDL_Renderer *renderer, SDL_Texture *outputTexture, SDL_Texture *firstInputTexture, SDL_Texture *secondInputTexture, SDL_Rect *firstInputCoord, SDL_Rect *secondInputCoord, SDL_Rect *firstInputSRect, SDL_Rect *secondInputSRect);
